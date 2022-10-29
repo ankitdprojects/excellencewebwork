849590c76b27d46d42d9a957402918377727d936
add0000 <ankitprojects2000@gmail.com>
2022-10-29 16:44:28 +0530

    routes

--- src/Error/ErrorPage.jsx
+++ src/Error/ErrorPage.jsx
@@ -16,7 +16,7 @@ const ErrorPage = () => {
             homepage.
           </p>
 
-          <NavLink to="/excellencewebwork">
+          <NavLink to="/excellencewebwork/">
             <Button>Go Back to Home</Button>
           </NavLink>
         </div>
